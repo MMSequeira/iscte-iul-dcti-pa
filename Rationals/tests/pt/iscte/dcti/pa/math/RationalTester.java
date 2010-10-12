6decf9e44bcd9f1ccf9195cbd559fce7c73ec480
MMSequeira@gmail.com <MMSequeira@gmail.com@672aaf38-5943-b58a-8c72-bd47800389a2>
2010-10-12 22:26:14 +0000

    Added new constructors and new tests. Tests fail!

--- Rationals/tests/pt/iscte/dcti/pa/math/RationalTester.java
+++ Rationals/tests/pt/iscte/dcti/pa/math/RationalTester.java
@@ -1,6 +1,8 @@
 package pt.iscte.dcti.pa.math;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -27,6 +29,47 @@ public class RationalTester {
 	public void tearDown() throws Exception {
 	}
 	
+	@Test
+	public void testEquals() {
+		Rational r1 = new Rational(1, 2);
+		Rational r2 = new Rational(-2, -4);
+		Rational r3 = new Rational(-2, 4);
+
+		assertTrue(r1.equals(r2));
+		assertTrue(r2.equals(r1));
+		assertFalse(r3.equals(r1));
+		assertFalse(r1.equals(r3));
+		assertTrue(r1.equals(r1));
+		assertFalse(r1.equals(null));
+		assertFalse(r1.equals("Hello!"));
+	}
+
+//	@Test
+//	public void testAdd???() {
+//		Rational r1 = new Rational(1, 3);
+//		Rational r2 = new Rational(4, 6);
+//
+//		r2.add(r1);
+//		assertEquals(0, r.getNumerator());
+//		assertEquals(1, r.getDenominator());
+//	}
+
+	@Test
+	public void testZeroArgumentsConstructor() {
+		Rational r = new Rational();
+
+		assertEquals(0, r.getNumerator());
+		assertEquals(1, r.getDenominator());
+	}
+
+	@Test
+	public void testOneArgumentsConstructor() {
+		Rational r = new Rational(2);
+
+		assertEquals(2, r.getNumerator());
+		assertEquals(1, r.getDenominator());
+	}
+
 	@Test
 	public void testTwoArgumentsConstructor() {
 		Rational r = new Rational(2, 4);
