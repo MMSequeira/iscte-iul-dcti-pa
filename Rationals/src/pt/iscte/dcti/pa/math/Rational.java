a47a4bc1dd18066bb2a98cc0d83a6f680dcdba7c
MMSequeira@gmail.com <MMSequeira@gmail.com@672aaf38-5943-b58a-8c72-bd47800389a2>
2010-10-12 20:29:28 +0000

    Initial import.

---
+++ Rationals/src/pt/iscte/dcti/pa/math/Rational.java
@@ -0,0 +1,69 @@
+package pt.iscte.dcti.pa.math;
+
+public class Rational {
+
+	private int numerator;
+	private int denominator;
+
+	public Rational(final int numerator, final int denominator) {
+		if(denominator == 0)
+			throw new IllegalArgumentException("Illegal value of denominator. Should be != 0, was " + denominator + ".");
+		
+		this.numerator = numerator;
+		this.denominator = denominator;
+
+		//normalize();
+		
+		checkInvariant();
+	}
+
+	public int getNumerator() {
+		checkInvariant();
+
+		return numerator;
+	}
+
+	public int getDenominator() {
+		checkInvariant();
+
+		return denominator;
+	}
+
+	private void checkInvariant() {
+		assert 0 < denominator : "Illegal state. Denominator should be > 0, is " + denominator + ".";
+		assert gcd(numerator, denominator) == 1 : "Illegal state. gcd(numerator, denominator) should be 1, is " + gcd(numerator, denominator) + ".";
+	}
+	
+	private void normalize() {
+		if(numerator == 0 && denominator == 0)
+			return;
+		
+		if(denominator < 0) {
+			numerator = -numerator;
+			denominator = -denominator;
+		}
+		
+		int gcd = gcd(numerator, denominator);
+		
+		numerator /= gcd;
+		denominator /= gcd;
+	}
+
+	private static int gcd(int firstValue, int secondValue) {
+		assert firstValue != 0 || secondValue != 0 : "IIlegal value of arguments. Cannot be both zero.";
+
+		while(firstValue != 0) {
+			final int oldFirstValue = firstValue;
+			firstValue = secondValue % firstValue;
+			secondValue = oldFirstValue;
+		}
+		
+		return secondValue;
+	}
+
+	// TODO Remove main from Rational class.
+	public static void main(final String[] arguments) {
+		new Rational(2, 4);
+	}
+
+}
