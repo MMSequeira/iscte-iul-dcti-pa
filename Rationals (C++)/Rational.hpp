e0ef388f714c809200cd1b49a5b7f366d37d90c2
MMSequeira@gmail.com <MMSequeira@gmail.com@672aaf38-5943-b58a-8c72-bd47800389a2>
2010-10-25 21:45:09 +0000

    Added operators == and +=. Added test code. Etc.

--- Rationals (C++)/Rational.hpp
+++ Rationals (C++)/Rational.hpp
@@ -3,40 +3,62 @@
 
 #include <cassert>
 
-class Rational {
+namespace pt {
+	namespace iscte {
+		namespace dcti {
+			namespace pa {
+				class Rational {
+				public:
+					Rational(int const numerator = 0, int const denominator = 1);
 
-public:
-	Rational(int const numerator = 0, int const denominator = 1);
+					int numerator() const;
 
-	int numerator();
+					int denominator() const;
 
-	int denominator();
+					void operator+=(Rational otherRational);
 
-private:
+					bool operator==(Rational otherRational) const;
 
-	static int gcd(int firstValue, int secondValue);
+				private:
 
-	void checkInvariant();
+					static int gcd(int firstValue, int secondValue);
 
-	int numerator_;
-	int denominator_;
-};
+					void reduce();
 
-inline int Rational::numerator() {
-	checkInvariant();
+					void checkInvariant() const;
 
-	return numerator_;
-}
+					int numerator_;
+					int denominator_;
+				};
 
-inline int Rational::denominator() {
-	checkInvariant();
+				inline int Rational::numerator() const {
+					checkInvariant();
 
-	return denominator_;
-}
+					return numerator_;
+				}
 
-inline void Rational::checkInvariant() {
-	assert(0 < denominator_);
-	assert(gcd(numerator_, denominator_) == 1);
+				inline int Rational::denominator() const {
+					checkInvariant();
+
+					return denominator_;
+				}
+
+				inline void Rational::checkInvariant() const {
+					assert(0 < denominator_);
+					assert(gcd(numerator_, denominator_) == 1);
+				}
+
+				inline void Rational::reduce() {
+					assert(numerator_ != 0 || denominator_ != 0);
+
+					int const greatest_common_divisor = gcd(numerator_, denominator_);
+
+					numerator_ /= greatest_common_divisor;
+					denominator_ /= greatest_common_divisor;
+				}
+			}
+		}
+	}
 }
 
 #endif /* RATIONAL_HPP_ */
