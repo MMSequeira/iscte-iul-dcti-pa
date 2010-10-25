843ab6cfebbe56e14b96ed2046e2212814859db7
MMSequeira@gmail.com <MMSequeira@gmail.com@672aaf38-5943-b58a-8c72-bd47800389a2>
2010-10-25 21:45:09 +0000

    Added operators == and +=. Added test code. Etc.

--- trunk/Rationals (C++)/Rational.cpp
+++ trunk/Rationals (C++)/Rational.cpp
@@ -1,6 +1,6 @@
 #include "Rational.hpp"
 
-Rational::Rational(int const numerator, int const denominator) :
+pt::iscte::dcti::pa::Rational::Rational(int const numerator, int const denominator) :
 	numerator_(numerator), denominator_(denominator) {
 	assert(denominator != 0);
 
@@ -9,15 +9,12 @@ Rational::Rational(int const numerator, int const denominator) :
 		denominator_ = -denominator;
 	}
 
-	int const greatest_common_divisor = gcd(numerator_, denominator_);
-
-	numerator_ /= greatest_common_divisor;
-	denominator_ /= greatest_common_divisor;
+	reduce();
 
 	checkInvariant();
 }
 
-int Rational::gcd(int firstValue, int secondValue) {
+int pt::iscte::dcti::pa::Rational::gcd(int firstValue, int secondValue) {
 	assert(firstValue != 0 || secondValue != 0);
 
 	if (firstValue < 0)
@@ -33,3 +30,51 @@ int Rational::gcd(int firstValue, int secondValue) {
 
 	return secondValue;
 }
+
+void pt::iscte::dcti::pa::Rational::operator+=(Rational otherRational) {
+	checkInvariant();
+
+	numerator_ = otherRational.denominator() * numerator() + denominator() * otherRational.numerator();
+	denominator_ *= otherRational.denominator();
+
+	reduce();
+
+	checkInvariant();
+}
+
+bool pt::iscte::dcti::pa::Rational::operator==(Rational otherRational) const {
+	checkInvariant();
+
+	return numerator() == otherRational.numerator() && denominator() == otherRational.denominator();
+}
+
+
+#include <iostream>
+
+using namespace std;
+using namespace pt::iscte::dcti::pa;
+
+int main() {
+	cout << "Starting tests." << endl;
+
+	cout << "Constructing r1." << endl;
+	Rational r1(3, 2);
+
+	cout << "Constructing r2." << endl;
+	Rational r2(1, 2);
+
+	cout << "Adding r2 to r1." << endl;
+	r1 += r2;
+
+	cout << "Assigning r1 to r2." << endl;
+	r2 = r1;
+
+	cout << "Checking correction of addition." << endl;
+	assert(r1 == 2);
+
+	Rational const r3(1, 3);
+
+	cout << r3.numerator() << endl;
+}
+
+
