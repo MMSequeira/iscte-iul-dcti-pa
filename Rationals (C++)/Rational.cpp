3f9a3f870417c9f8c0b9cc6e57d56c14e4bdcb48
MMSequeira@gmail.com <MMSequeira@gmail.com@672aaf38-5943-b58a-8c72-bd47800389a2>
2010-10-26 21:42:28 +0000

    Added +, suffix and prefix ++, etc. Several other improvements.

--- Rationals (C++)/Rational.cpp
+++ Rationals (C++)/Rational.cpp
@@ -31,7 +31,7 @@ int pt::iscte::dcti::pa::Rational::gcd(int firstValue, int secondValue) {
 	return secondValue;
 }
 
-void pt::iscte::dcti::pa::Rational::operator+=(Rational otherRational) {
+pt::iscte::dcti::pa::Rational& pt::iscte::dcti::pa::Rational::operator+=(Rational const& otherRational) {
 	checkInvariant();
 
 	numerator_ = otherRational.denominator() * numerator() + denominator() * otherRational.numerator();
@@ -40,12 +40,54 @@ void pt::iscte::dcti::pa::Rational::operator+=(Rational otherRational) {
 	reduce();
 
 	checkInvariant();
+
+	return *this;
 }
 
-bool pt::iscte::dcti::pa::Rational::operator==(Rational otherRational) const {
+pt::iscte::dcti::pa::Rational& pt::iscte::dcti::pa::Rational::operator++() {
+	checkInvariant();
+
+	//numerator_ = numerator() + denominator();
+	//checkInvariant();
+	//return *this;
+
+	//return *this += 1;
+
+	return operator+=(1);
+}
+
+pt::iscte::dcti::pa::Rational pt::iscte::dcti::pa::Rational::operator++(int) {
+	checkInvariant();
+
+	Rational const copy = *this;
+
+	++*this;
+
+	return copy;
+	//return Rational(copy);
+	// TODO Check the reason for the warning!
+}
+
+bool pt::iscte::dcti::pa::Rational::operator==(Rational const& otherRational) const {
 	checkInvariant();
 
 	return numerator() == otherRational.numerator() && denominator() == otherRational.denominator();
+}
+
+bool pt::iscte::dcti::pa::Rational::operator<(Rational const& otherRational) const {
+	checkInvariant();
+
+	return numerator() * otherRational.denominator() < otherRational.numerator() * denominator();
+}
+
+bool pt::iscte::dcti::pa::Rational::operator>(Rational const& otherRational) const {
+	checkInvariant();
+
+	return otherRational < *this;
+}
+
+pt::iscte::dcti::pa::Rational pt::iscte::dcti::pa::operator+(Rational firstRational, Rational const& secondRational) {
+	return firstRational += secondRational;
 }
 
 
@@ -69,12 +111,30 @@ int main() {
 	cout << "Assigning r1 to r2." << endl;
 	r2 = r1;
 
-	cout << "Checking correction of addition." << endl;
+	cout << "Checking correction of accumulation." << endl;
 	assert(r1 == 2);
+
+	cout << "Checking correction of addition." << endl;
+	assert(r1 + r2 == 4);
 
 	Rational const r3(1, 3);
 
 	cout << r3.numerator() << endl;
+
+	Rational r4 = r3;
+
+	Rational r5 = ++r4;
+	cout << "Checking correction of prefix increment." << endl;
+	assert(r4 == r3 + 1);
+	assert(r5 == r4);
+
+	Rational r6 = r3;
+	Rational r7 = r6++;
+	cout << "Checking correction of suffix increment." << endl;
+	assert(r6 == r3 + 1);
+	assert(r7 == r3);
+
+	(r6++)++;
 }
 
 
