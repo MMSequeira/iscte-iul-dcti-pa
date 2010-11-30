4b5bd5484053a74aba709793610b1911d34ddf27
MMSequeira@gmail.com <MMSequeira@gmail.com@672aaf38-5943-b58a-8c72-bd47800389a2>
2010-11-30 22:10:49 +0000

    Multithreading supported. Problem of missed stack elements detected.

---
+++ trunk/StackReplica/src/pt/iscte/dcti/pa/C.java
@@ -0,0 +1,8 @@
+package pt.iscte.dcti.pa;
+
+public class C {
+
+	public static void run(Runnable runnable) {
+		runnable.run();
+	}
+}
--- trunk/StackReplica/src/pt/iscte/dcti/pa/Tester.java
+++ trunk/StackReplica/src/pt/iscte/dcti/pa/Tester.java
@@ -7,8 +7,15 @@ import java.util.Arrays;
 public class Tester {
 
 	public static void main(final String[] arguments) {
+//		final B b = new B();
+//		new Thread(new Runnable() {
+//			public void run() {
+////				new B().foo1();
+//				b.foo1();
+//			}
+//		}).start();
 		new A().foo1();
-		new A().foo2();
+//		new A().foo2();
 		
 	}
 }
